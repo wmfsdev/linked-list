4d7dba9dbbaad9acc00fcfc27ba601fa92e431f8
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2023-09-03 22:45:15 +0100

    intial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const LinkedList = (node) => {
+
+    const append = (value) => {}
+    const prepend = (value) => {}
+    const size = () => {}
+    const head = () => {}
+    const tail = () => {}
+    const at = (index) => {}
+    const pop = () => {}
+    const contains = (value) => {}
+    const find = (value) => {}
+    const toString = () => {}
+
+    { return node }
+}
+
+
+const node = (value = null, nextNode = null) => {
+
+    return { value, nextNode }
+
+}
+
+let newNode = node(42)
+
+let newList = LinkedList(newNode)
+
+console.log(newList)
